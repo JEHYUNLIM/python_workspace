34b4ae1d5c6f6a40f32564aa2ed39160299b690c
JEHYUNLIM <ljehyun@gmail.com>
2019-04-04 08:59:13 +0900

    Upload Algorithm study 20190404

--- BaekjoonCode/10828.py
+++ BaekjoonCode/10828.py
@@ -1,2 +1,24 @@
-N = input().split()
-print(N)
+import sys
+N = int(input())
+r = []
+for i in range(N):
+	word = sys.stdin.readline().split()
+	if word[0]=="push":
+		r.append(word[1])
+	elif word[0]=="top":
+		if len(r)==0:
+			print(-1)
+		else:
+			print(r[len(r)-1])
+	elif word[0]=="size":
+		print(len(r))
+	elif word[0]=="empty":
+		if len(r)==0:
+			print(1)
+		else:
+			print(0)
+	elif word[0]=="pop":
+		if len(r)==0:
+			print(-1)
+		else:
+			print(r.pop())
